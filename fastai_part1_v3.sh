609c4d21e2ae9076fdd5bf58daa1a6e1c234044b
noklam <channoklam@yahoo.com.hk>
2018-10-21 01:34:37 +0800

    Update fastai_part1_v3.sh

--- fastai_part1_v3.sh
+++ fastai_part1_v3.sh
@@ -1,21 +1,26 @@
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
 bash Anaconda3-5.3.0-Linux-x86_64.sh  -b
+
+sleep 3
 echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
-conda create -n fastai
-sleep 5
 
+git clone https://github.com/fastai/fastai.git
+cd fastai/
+conda create -n "fastai"
+
+sleep 5
 echo 'source activate fastai' >> ~/.bashrc
 source activate fastai
 source ~/.bashrc
 
+
 conda install -c pytorch pytorch-nightly cuda92
 conda install -c fastai torchvision-nightly
 conda install -c fastai fastai
-git clone https://github.com/fastai/fastai.git
-cd fastai/
 
 jupyter notebook --generate-config
 echo "c.NotebookApp.ip = '0.0.0.0'" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
+
