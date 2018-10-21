ab75874d25bcbb69e2e58eb24af3e22163ee7e4e
noklam <channoklam@yahoo.com.hk>
2018-10-21 15:23:58 +0800

    Update setup_conda.sh

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,5 +1,6 @@
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
 bash Anaconda3-5.3.0-Linux-x86_64.sh  -b
+sudo apt-get update
 
 sleep 5
 echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
